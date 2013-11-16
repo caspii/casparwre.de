d108c8afa6a14e1a712eb386b77af373765890b6
caspii <caspar.wrede@gmail.com>
2013-11-16 18:38:16 +0100

    Ongoing development

--- initializr/fortune.php
+++ initializr/fortune.php
@@ -1,8 +1,15 @@
 <?php
-  $arr = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
-  header('Content-Type: application/json');
-  echo json_encode($arr); 
-  //echo "<br>"; 
-  //echo shell_exec('/usr/games/fortune');
-
+	// This function returns an array of fortunes
+	header('Content-Type: application/json');
+  	$noFortunes = 5;	
+  	$data = array();
+  
+  
+	for ($x=0; $x<$noFortunes; $x++) {
+		$fortune = shell_exec('/usr/games/fortune -s'); // Execute 'fortune' command on the server
+		$fortune = str_replace("\n","<br>",$fortune); // Replace newline
+		$fortune = str_replace("\t","<nbsp><nbsp><nbsp>",$fortune); // Replace tabs		
+		array_push($data, $fortune);
+	}  
+  	echo json_encode($data); 
 ?>
--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,3 +1,34 @@
+var clicks=0;
+var fortunes;	
+
 $(document).ready(function(){
-	console.log("hello world")
+	
+	console.log("hello world!")
+	$("#fortune-btn").hide();
+		
+	// Get arrary of fortunes
+	$.get( "fortune.php", function( data ) {
+		fortunes = data;
+		$("#fortune").html(fortunes[0]);
+		$("#fortune-btn").show();
+	}, "json");
+
+	$("#fortune-btn").click(changeFortune);
 });
+
+
+function changeFortune() {
+	clicks++;
+	console.log("clicks=" + clicks );
+	$("#fortune-btn").html("Again &raquo;");
+	$("#fortune").hide();
+	$("#fortune").html(fortunes[clicks]);
+	$("#fortune").fadeIn();
+	
+	if (clicks==fortunes.length) {
+		$("#fortune").html("Enough");
+		$("#fortune-btn").html("Sorry &raquo;");
+		$("#fortune-btn").attr("disabled", true);
+	}	
+	
+}
