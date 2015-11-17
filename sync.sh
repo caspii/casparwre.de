e57ada2ba33f5afb4df32938b6290df11858e26e
caspii <caspar.wrede@gmail.com>
2015-11-17 17:28:37 +0100

    not required

--- sync.sh
+++
@@ -1,6 +0,0 @@
-# Generate blog first
-./generate-blog.sh
-
-# Deploy to server
-TARGET=casparwre.de:/home/wrede/websites/casparwre.de
-rsync -av --exclude='.git/' --exclude='*~' --exclude='staging/' ./ $TARGET
