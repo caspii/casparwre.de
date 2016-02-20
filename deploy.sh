b36df00239ae9a9dcb255ddd3fa819a4d157a93c
caspii <caspar.wrede@gmail.com>
2016-02-20 23:59:17 +0100

    Fixed url

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-# Generate blog first
-./generate-blog.sh
+jekyll b
 
 # Deploy to server
-TARGET=wrede@46.101.241.225:/home/wrede/websites/casparwre.de
-rsync -av --exclude='.git/' --exclude='*~' --exclude='staging/' --exclude='.c9/' ./ $TARGET
+TARGET=wrede@casparwre.de:/home/wrede/websites/casparwre.de
+rsync -av --exclude='.git/' --exclude='*~' --exclude='staging/' --exclude='.c9/' ./_site/ $TARGET
